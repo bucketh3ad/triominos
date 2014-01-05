a0c02eb564250342d27a2800a48458352aa93478
bucketh3ad <scrippman@gmail.com>
2014-01-05 13:48:56 -0700

    Add functions to handle invalid input

--- tri.py
+++ tri.py
@@ -2,9 +2,27 @@ playerCount = 0
 players = []
 scores = {}
 
+def inputInt(prompt):
+    while True:
+        x = input(prompt)
+        if x.isdigit():
+            return int(x)
+        else:
+            print("Invalid Input!")
+
+def inputScore(prompt):
+    while True:
+        x = input(prompt)
+        if x.isdigit():
+            return int(x), False
+        elif x[-1] == "w" and x[:-1].isdigit():
+            return int(x[:-1]), True
+        else:
+            print("Invalid Input!")
+
 def newGame():
     global playerCount
-    playerCount = int(input("Enter the number of players: "))
+    playerCount = inputInt("Enter the number of players: ")
     for i in range(playerCount):
         players.append(input("Enter the name for player " + str(1+i) + ": "))
     for player in players:
@@ -16,24 +34,23 @@ def showScores():
 
 def startRound():
     showScores()
-    firstPlayer = int(input("Enter the number of the first player: "))-1
+    firstPlayer = inputInt("Enter the number of the first player: ")-1
     currentPlayer = firstPlayer
     scores[players[firstPlayer]] += 10
     winner = False
     winlist = []
     while not winner:
-        newscore = input("Enter the score for " + players[currentPlayer] + ": ")
-        if newscore[-1] == "w":
-            winner = True
-            newscore = int(newscore[:-1]) + 25
-        scores[players[currentPlayer]] += int(newscore)
+        newscore,winner = inputScore("Enter the score for " + players[currentPlayer] + ": ")
+        scores[players[currentPlayer]] += newscore
+        if winner:
+            scores[players[currentPlayer]] += 25
         if scores[players[currentPlayer]] >= 400:
             winlist.append(players[currentPlayer])
         showScores()
         if not winner:
             currentPlayer = (currentPlayer + 1) % playerCount
     while winner:
-        newscore = int(input("Enter a subtotal or 0 to finish: "))
+        newscore = inputInt("Enter a subtotal or 0 to finish: ")
         scores[players[currentPlayer]] += newscore
         if newscore == 0:
             showScores()
